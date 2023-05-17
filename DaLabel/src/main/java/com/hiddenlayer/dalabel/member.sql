5576f31854abe05982049f60c9bc9147bc9dd8c3
sdedu <sdedu@DESKTOP-8TM3SJ3>
2023-05-17 11:11:11 +0900

    member DB table 이름 길이 좀 늘렸습니다..영어 이름을 생각을 못했네요

--- DaLabel/src/main/java/com/hiddenlayer/dalabel/member.sql
+++ DaLabel/src/main/java/com/hiddenlayer/dalabel/member.sql
@@ -1,10 +1,12 @@
 create table member (
 	user_id varchar2(15 char) primary key,
 	user_email varchar2(15 char),
-	user_name varchar2(5 char) not null,
+	user_name varchar2(20 char) not null,
 	user_pw varchar2(15 char) not null,
 	user_birth date not null,
 	user_img varchar2(100 char),
 	user_isdeleted number(1) not null, -- 0이 탈퇴 안한거, 1이 탈퇴한거
 	user_deleted_date date
 )
+
+select * from member
