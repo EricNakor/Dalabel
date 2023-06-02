2a86a8f83423131291f06ac79c83662546623838
sdedu <sdedu@DESKTOP-C31G78I>
2023-06-02 15:38:56 +0900

    .

--- DaLabel/src/main/java/com/hiddenlayer/dalabel/HomeController.java
+++ DaLabel/src/main/java/com/hiddenlayer/dalabel/HomeController.java
@@ -10,31 +10,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
- * Handles requests for the application home page.
- */
 @Controller
 public class HomeController {
 	
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-
 		model.addAttribute("serverTime", "Hello world! 한글테스트" );
-		
 		return "home";
-	}
-	
-	@RequestMapping(value = "/goLabelling", method = RequestMethod.GET)
-	public String goLabelling(HttpServletRequest req, HttpServletResponse res) {
-		return "labelling/goLabelling";
-	}
-	
-	@RequestMapping(value = "/doLabelling", method = RequestMethod.GET)
-	public String doLabelling(HttpServletRequest req, HttpServletResponse res) {
-		return "labelling/doLabelling";
 	}
 }
