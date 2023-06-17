db419f28ace0d5478fc9ad532256a87d91b0ca1c
신용대 <@175.210.74.136>
2023-06-17 22:40:25 +0900

    갑자기 업로드 안되는거 해결

--- DaLabel/src/main/java/com/hiddenlayer/dalabel/util/UnZipperThread.java
+++ DaLabel/src/main/java/com/hiddenlayer/dalabel/util/UnZipperThread.java
@@ -62,7 +62,7 @@ public class UnZipperThread extends Thread {
 				fileName = fileInfo.getFileName();
 				endtag = fileInfo.getEndtag();
 				bundleNo = fileInfo.getBundleNumber();
-				folderName = fileName.substring(fileName.indexOf("_") + 1, fileName.length() - 4);
+				folderName = fileName.substring(0, fileName.length() - 4);
 				try {
 					File zipFile = new File(fileName);
 					zis = new ZipInputStream(new BufferedInputStream(new FileInputStream(zipFile)));
@@ -76,7 +76,8 @@ public class UnZipperThread extends Thread {
 							continue;
 						}
 						idx++;
-						String innerFileName = UUID.randomUUID().toString()+"_"+String.format("%08d", idx) + "." + fileEnd;
+						String innerFileName = UUID.randomUUID().toString() + "_" + String.format("%08d", idx) + "."
+								+ fileEnd;
 						file = new File(folderName, innerFileName);
 						if (!ze.isDirectory()) {
 							try {
