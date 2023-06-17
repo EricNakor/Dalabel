6433e0f3b33520584503d31476fdf2a0333cc729
신용대 <@175.210.74.136>
2023-06-17 22:43:57 +0900

    test

--- DaLabel/src/main/webapp/resources/js/move.js
+++ DaLabel/src/main/webapp/resources/js/move.js
@@ -1,7 +1,5 @@
-function deleteBundle(e) {
+function deleteBundle(no) {
 	if (confirm("정말 삭제하시겠습니까?")) {
-		return true;
-	} else {
-		return false;
+		location.href = "delete.needlogin.bundle?bundle_no=" + no
 	}
 }
