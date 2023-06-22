d4de1e2717ee38b3fd254ae77aabfbd13a4f8635
sdedu <sdedu@DESKTOP-16PQFK6>
2023-06-22 20:17:12 +0900

    Add file download controller

--- DaLabel/src/main/java/com/hiddenlayer/dalabel/manageLabeling/ManageLabelingDAO.java
+++ DaLabel/src/main/java/com/hiddenlayer/dalabel/manageLabeling/ManageLabelingDAO.java
@@ -1,12 +1,17 @@
 package com.hiddenlayer.dalabel.manageLabeling;
 
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.hiddenlayer.dalabel.dolabeling.LabelDoList;
 import com.hiddenlayer.dalabel.manageBundle.ManageSelector;
@@ -18,10 +23,10 @@ public class ManageLabelingDAO {
 	// 등록 - 데이터 선택해서 오픈하기.
 	@Autowired
 	private SqlSession ss;
-	
+
 	@Autowired
 	private ProjectSession ps;
-	
+
 	@Autowired
 	private PageOption po;
 
@@ -53,26 +58,53 @@ public class ManageLabelingDAO {
 				.getMyDeatilProject(Integer.parseInt(req.getParameter("project_no")));
 		req.setAttribute("project", project);
 	}
-	
+
 	// 프로젝트 권한설정 관리
 	public void updateProjectAccessLevel(LabelingProject lp, HttpServletRequest req) {
 		ss.getMapper(ManageLabelingMapper.class).updateProjectAccessLevel(lp);
-		
-		if(lp.getProject_access_level().intValue()!=0) {
-			ps.createDoLabeling(lp.getProject_no(), 
-					ss.getMapper(ManageLabelingMapper.class).getFileCount(lp),
+
+		if (lp.getProject_access_level().intValue() != 0) {
+			ps.createDoLabeling(lp.getProject_no(), ss.getMapper(ManageLabelingMapper.class).getFileCount(lp),
 					lp.getProject_access_level());
-		}else {
+		} else {
 			ps.terminateDoLabeling(lp.getProject_no());
 		}
 	}
+
 	// 정산시작하기
 	// 참여인원 관리 - 조회, 대기, 수락, 거부, 밴, (초대)
 	public void select(int no, HttpServletRequest req) {
 		req.setAttribute("mlu", ss.getMapper(ManageLabelingMapper.class).selectLabelingUser(no)); // 모든 대기다 명단이 나옴
 	}
+
 //	
 	public void changeUserAccess(LabelDoList ld, HttpServletRequest req) {
 		ss.getMapper(ManageLabelingMapper.class).changeUserAccess(ld); // 수락, 거부, 밴할 때 호출할 함수
+	}
+
+	public void downloadFile(HttpServletRequest req, int project_no, HttpServletResponse response) {
+		String userid = (String)req.getSession().getAttribute("loginUserID");
+		ArrayList<LabelingResult> results = ss.getMapper(ManageLabelingMapper.class).getResults(project_no, userid);
+		System.out.println(results.size());
+		OutputStream os = null;
+		response.setHeader("Content-Disposition", "attachment;filename=" + "result.csv"+ ";");
+
+		try {
+			os = response.getOutputStream();
+			for (LabelingResult labelingResult : results) {
+				os.write(String.format("%s,%s\n", labelingResult.getData_no(), labelingResult.getLabel_result()).getBytes());
+			}
+			os.flush();
+			os.close();
+			response.flushBuffer();
+		} catch (Exception e) {
+			try {
+				os.close();
+			} catch (Exception e2) {
+				// TODO: handle exception
+			}
+			// TODO: handle exception
+		}
+
 	}
 }
